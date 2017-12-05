8e0f5edc55edee73086bc53b16e9106093647581
Steven Ham <stevenlanceham@gmail.com>
2017-12-05 12:02:05 -0500

    Styled subscriber list

--- src/components/subscriber-list.js
+++ src/components/subscriber-list.js
@@ -1,27 +1,45 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Table from 'rc-table';
+import styled from 'styled-components';
+import 'rc-table/assets/index.css';
 
 const SubscriberList = ({ subscribers }) => {
-  // const columns = [{
-  //   title: 'User', dataIndex: 'magazineName', key: 'magazineName', width: 100,
-  // }, {
-  //   title: 'Qualities', dataIndex: 'keywords', key: 'qualities', width: 200,
-  // }];
+  const columns = [{
+    title: 'User', dataIndex: 'retObj.username', key: 'User', width: 100,
+  }, {
+    title: 'Magazines', dataIndex: 'retObj.magazines', key: 'Magazines', width: 200,
+  }];
 
-    console.log(`SubscriberList.subscribers: ${JSON.stringify(subscribers)}`);
-    const subscriberItems = subscribers.map(subscriber => (
-    <li key={subscriber.id}>
-        <span className="subscriber-text">
-          {subscriber.retObj.username}: {subscriber.retObj.magazines}
-          {console.log(`Subscriber${subscriber.id}: ${JSON.stringify(subscriber)}`)}
-        </span>
-    </li>
-  ));
+  // console.log(`SubscriberList.subscribers: ${JSON.stringify(subscribers)}`);
+  // const subscriberItems = subscribers.map(subscriber => (
+  //   <li key={subscriber.id}>
+  //       <span className="subscriber-text">
+  //         {subscriber.retObj.username}: {subscriber.retObj.magazines}
+  //         {console.log(`Subscriber${subscriber.id}: ${JSON.stringify(subscriber)}`)}
+  //       </span>
+  //   </li>
+  // ));
+
+  const BodyRow = styled.tr`
+  &:hover {
+    background: paleblue !important;
+  }
+  `;
+  
+    const components = {
+      body: {
+        row: BodyRow,
+      },
+    };
+
 
   return (
-    <ul>
-      {subscriberItems}
-    </ul>
+    // <ul>
+    //   {subscriberItems}
+    // </ul>
+    <Table columns={columns} data={subscribers} components={components} />
+    
   );
 };
 
